8dcb86278e83eb5fd7ee069e6192a035ae1ebcc1
Priyal lohar <priyallohar27@gmail.com>
2023-08-05 17:40:17 +0530

    Done excersize 1, 2

---
+++ exercises/building-a-form.js
@@ -0,0 +1,4 @@
+import React  from 'react'
+import { KeyboardAvoidingView, Platform, StyleSheet, Text, TextInput, TouchableOpacity, View, Keyboard, ScrollView  } from 'react-native'
+
+
--- exercises/capturing-taps.js
+++ exercises/capturing-taps.js
@@ -1,9 +1,23 @@
+import React from 'react'
+import { Alert, Button } from 'react-native'
+import { View } from 'react-native-web'
+
+const buttonclickHandlerr = ()=>{
+   Alert.alert("Alert","hello!")
+}
+
+const capturingTaps = () => {
+  return (
+    <View>
+   <Button title='Button 1' onPress={buttonclickHandlerr}></Button>
+   <Button title='Button 2' onPress={()=>alert("hello 2")}></Button>
+   </View>
+  )
+}
+
 // Capturing User Taps
 
 // TIP: Button is a component exposed by React Native. You'll import it the same way you did View
 // and Text previously.
 
-export default () => {
-  // Replace return null with your code.
-  return null;
-};
+export default capturingTaps
